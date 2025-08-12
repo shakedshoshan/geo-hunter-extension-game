6380632d8dddc11a547d64b8f85e5ed33a0a8cd8
Shaked Shoshan <shakedshoshan8@gmail.com>
2025-08-12 22:03:33 +0000

    search in the internet to get all  country data . there is few country o

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -110,5 +110,71 @@ export const countries: Country[] = [
     code: "au",
     flag: "https://flagcdn.com/w320/au.png",
     ranks: { population: 55, gdp: 13, area: 6, hdi: 5, coffee: 35, crime: 72, tourism: 40, research: 11, renewable: 10, literacy: 1, tech: 15, car_production: 30, airports: 25, water: 20, forest: 12, universities: 9, manufacturing: 22, coastline: 5, agriculture: 14, health: 32, education: 8 },
+  },
+  {
+    name: "United Kingdom",
+    code: "gb",
+    flag: "https://flagcdn.com/w320/gb.png",
+    ranks: { population: 21, gdp: 6, area: 78, hdi: 18, coffee: 110, crime: 69, tourism: 10, research: 7, renewable: 12, literacy: 1, tech: 6, car_production: 16, airports: 3, water: 50, forest: 140, universities: 2, manufacturing: 10, coastline: 11, agriculture: 30, health: 18, education: 13 },
+  },
+  {
+    name: "France",
+    code: "fr",
+    flag: "https://flagcdn.com/w320/fr.png",
+    ranks: { population: 20, gdp: 7, area: 49, hdi: 28, coffee: 115, crime: 71, tourism: 1, research: 6, renewable: 18, literacy: 1, tech: 9, car_production: 10, airports: 9, water: 40, forest: 80, universities: 10, manufacturing: 7, coastline: 35, agriculture: 6, health: 8, education: 22 },
+  },
+  {
+    name: "South Korea",
+    code: "kr",
+    flag: "https://flagcdn.com/w320/kr.png",
+    ranks: { population: 29, gdp: 12, area: 109, hdi: 19, coffee: 30, crime: 22, tourism: 27, research: 8, renewable: 48, literacy: 1, tech: 7, car_production: 7, airports: 14, water: 90, forest: 120, universities: 15, manufacturing: 6, coastline: 80, agriculture: 25, health: 17, education: 11 },
+  },
+  {
+    name: "Indonesia",
+    code: "id",
+    flag: "https://flagcdn.com/w320/id.png",
+    ranks: { population: 4, gdp: 16, area: 15, hdi: 114, coffee: 4, crime: 85, tourism: 25, research: 45, renewable: 30, literacy: 110, tech: 22, car_production: 15, airports: 16, water: 6, forest: 8, universities: 80, manufacturing: 11, coastline: 3, agriculture: 4, health: 140, education: 110 },
+  },
+  {
+    name: "Nigeria",
+    code: "ng",
+    flag: "https://flagcdn.com/w320/ng.png",
+    ranks: { population: 6, gdp: 27, area: 32, hdi: 163, coffee: 25, crime: 140, tourism: 100, research: 70, renewable: 80, literacy: 160, tech: 40, car_production: 40, airports: 70, water: 15, forest: 90, universities: 150, manufacturing: 30, coastline: 150, agriculture: 8, health: 187, education: 161 },
+  },
+  {
+    name: "Mexico",
+    code: "mx",
+    flag: "https://flagcdn.com/w320/mx.png",
+    ranks: { population: 10, gdp: 15, area: 14, hdi: 76, coffee: 8, crime: 125, tourism: 7, research: 30, renewable: 25, literacy: 95, tech: 17, car_production: 6, airports: 17, water: 22, forest: 65, universities: 40, manufacturing: 13, coastline: 13, agriculture: 11, health: 61, education: 76 },
+  },
+  {
+    name: "Turkey",
+    code: "tr",
+    flag: "https://flagcdn.com/w320/tr.png",
+    ranks: { population: 18, gdp: 19, area: 37, hdi: 48, coffee: 90, crime: 80, tourism: 6, research: 40, renewable: 28, literacy: 70, tech: 25, car_production: 13, airports: 13, water: 55, forest: 55, universities: 50, manufacturing: 16, coastline: 17, agriculture: 13, health: 51, education: 49 },
+  },
+  {
+    name: "Spain",
+    code: "es",
+    flag: "https://flagcdn.com/w320/es.png",
+    ranks: { population: 30, gdp: 14, area: 52, hdi: 27, coffee: 120, crime: 40, tourism: 3, research: 14, renewable: 15, literacy: 30, tech: 19, car_production: 9, airports: 11, water: 65, forest: 70, universities: 35, manufacturing: 17, coastline: 25, agriculture: 16, health: 7, education: 23 },
+  },
+  {
+    name: "Argentina",
+    code: "ar",
+    flag: "https://flagcdn.com/w320/ar.png",
+    ranks: { population: 33, gdp: 26, area: 8, hdi: 47, coffee: 55, crime: 110, tourism: 45, research: 42, renewable: 24, literacy: 25, tech: 30, car_production: 20, airports: 35, water: 16, forest: 35, universities: 45, manufacturing: 25, coastline: 24, agriculture: 18, health: 75, education: 46 },
+  },
+  {
+    name: "Egypt",
+    code: "eg",
+    flag: "https://flagcdn.com/w320/eg.png",
+    ranks: { population: 14, gdp: 32, area: 30, hdi: 97, coffee: 130, crime: 90, tourism: 35, research: 55, renewable: 50, literacy: 130, tech: 45, car_production: 38, airports: 55, water: 150, forest: 190, universities: 90, manufacturing: 35, coastline: 85, agriculture: 22, health: 63, education: 98 },
+  },
+  {
+    name: "South Africa",
+    code: "za",
+    flag: "https://flagcdn.com/w320/za.png",
+    ranks: { population: 24, gdp: 36, area: 25, hdi: 109, coffee: 38, crime: 145, tourism: 55, research: 35, renewable: 60, literacy: 98, tech: 35, car_production: 22, airports: 45, water: 100, forest: 160, universities: 60, manufacturing: 28, coastline: 70, agriculture: 28, health: 175, education: 143 },
   }
 ];
